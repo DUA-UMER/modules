b1e8e394b9875f610d897eeb4ce1e2ec902f995b
DUA-UMER <161318323+DUA-UMER@users.noreply.github.com>
2024-03-25 17:10:23 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,9 @@
+import sLogin from "./student.js";
+import tLogin from "./teacher.js";
+
+let teacher = new tLogin();
+console.log(teacher.data)
+
+let student = new sLogin();
+console.log(student.data)
+
